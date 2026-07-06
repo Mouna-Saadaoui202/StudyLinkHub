542e44f6b5de93bd6760ddaf97c42b64fb4ed2f7
Mouna-Saadaoui202 <mouna.saadaoui202@gmail.com>
2026-07-06 14:58:09 +0100

    Configure GitHub Pages deployment

--- src/components/videos/VideoPlayer.jsx
+++ src/components/videos/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
-import video from '../../assets/img/stydylink modifier.mp4'
+const defaultVideoSrc = `${import.meta.env.BASE_URL}stydylink%20modifier.mp4`;
 
 export const VideoPlayer = ({ trigger, src }) => {
   return (
@@ -14,7 +14,7 @@ export const VideoPlayer = ({ trigger, src }) => {
         lockScroll
       >
         <video
-  src={video}
+  src={src || defaultVideoSrc}
   width="100%"
   height="600px"
   style={{ display: "block", position: "relative",background:"black" }}
